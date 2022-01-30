768db1879bbea564b7e4703c7cc1e10a5cec30e1
biancasofia <69814402+biancasofia@users.noreply.github.com>
2022-01-30 15:27:41 -0300

    Criando opções para o select

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/img'))
 app.get('', function (req, res) {
 
     res.sendFile(__dirname + "/public/index.html");
---
+++ public/js/itensSelect.js
@@ -0,0 +1,150 @@
+
+
+
+//gerar as opções pro select
+function geraOptions(vet, id) {
+
+    let optionSelect = document.getElementById(id)
+    for (let i = 0; i < vet.length; i++) {
+        let option = document.createElement("option")
+        option.value = vet[i]
+        option.text = vet[i]
+        optionSelect.appendChild(option)
+
+    }
+
+}
+
+function listarArrays() {
+
+    var idDinastia = "select-dinastia";
+    var idNome = "select-nome";
+    var idCidadeNatal = "select-cidade";
+    var idCausaMorte = "select-causa";
+    var dinastia = ['Constantinian',
+        'Gordian',
+        'Julio-Claudian',
+        'Flavian',
+        'Severan',
+        'Nerva-Antonine',
+        'Valentinian',
+        'Theodosian']
+    var nome = ['Galerius',
+        'Lucinius',
+        'Philip I',
+        'Trebonianus Gallus',
+        'Balbinus',
+        'Aurelian',
+        'Numerian',
+        'Carinus',
+        'Tiberius',
+        'Galba',
+        'Didius Julianus',
+        'Geta',
+        'Carus',
+        'Julian',
+        'Constans',
+        'Augustus',
+        'Trajan Decius',
+        'Tacitus',
+        'Probus',
+        'Elagabalus',
+        'Maximinus I',
+        'Gallienus',
+        'Gordian I',
+        'Marcus Aurelius',
+        'Domitian',
+        'Vitellius',
+        'Constantius I',
+        'Hostilian',
+        'Caracalla',
+        'Claudius Gothicus',
+        'Constantine II',
+        'Jovian',
+        'Gordian II',
+        'Pupienus',
+        'Constantine the Great',
+        'Severus Alexander',
+        'Diocletian',
+        'Valens',
+        'Valentinian II',
+        'Valentinian I',
+        'Theodosius I',
+        'Vespasian',
+        'Commodus',
+        'Pertinax',
+        'Hadrian',
+        'Trajan',
+        'Otho',
+        'Macrinus',
+        'Quintillus',
+        'Florian',
+        'Nero',
+        'Vetranio',
+        'Severus II',
+        'Claudius',
+        'Lucius Verus',
+        'Septimus Severus',
+        'Antonius Pius',
+        'Nerva',
+        'Caligula',
+        'Titus',
+        'Gratian',
+        'Maximian',
+        'Valerian',
+        'Aemilian',
+        'Maximinus II',
+        'Maxentius',
+        'Gordian III',
+        'Constantius II'];
+    var cidadeNatal = ['Romuliana',
+        'Felix Romuliana',
+        'Shahba',
+        '',
+        'Sirmium',
+        'Rome',
+        'Terracina',
+        'Milan',
+        'Narbo',
+        'Constantinople',
+        'Budalia',
+        'Interamna Nahars',
+        'Emesa',
+        'Dardania',
+        'Lugdunum',
+        'Arelate',
+        'Singidunum',
+        'Naissus',
+        'Arca Caesarea',
+        'Salona',
+        'Cibalae',
+        'Cauca',
+        'Falacrine',
+        'Lanuvium',
+        'Alba',
+        'Italica',
+        'Terentinum',
+        'Iol Caesarea',
+        'Antitum',
+        'Leptis Magna',
+        'Narni',
+        'Sirmium'];
+    var causaMorte = ['Natural Causes',
+        'Execution',
+        'Assassination',
+        'Unknown',
+        'Died in Battle',
+        'Suicide',
+        'Captivity'];
+    geraOptions(dinastia, idDinastia);
+    geraOptions(nome, idNome);
+    geraOptions(cidadeNatal, idCidadeNatal);
+    geraOptions(causaMorte, idCausaMorte);
+}
+
+
+listarArrays()
+
+
+
+
